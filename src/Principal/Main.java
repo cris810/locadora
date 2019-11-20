6a55dbcc1ba85962a26c2b22b4dcdd632484e45c
cris810 <gerlaniasantana33@gmail.com>
2019-11-20 09:13:26 -0300

    Fizemos login e menu

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -10,7 +10,7 @@ package Principal;
  * @author Aluno
  */
 public class Main {
-    public static void main(string[] args){
+    public static void main(String[] args){
         new Login().setVisible(true);
         
     }
